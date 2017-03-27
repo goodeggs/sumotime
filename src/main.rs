78f718e523bb7508cd1abee3e7ca8b3cb8d55eb9
Bob Zoller <bob@zoller.us>
2017-03-27 08:03:36 -1000

    logging to sumo

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,20 @@ extern crate getopts;
 extern crate wait_timeout;
 #[macro_use]
 extern crate version;
+extern crate hyper;
+extern crate hyper_native_tls;
 
+use std::io::Read;
 use std::process;
 use std::process::Command;
 use wait_timeout::ChildExt;
 use getopts::Options;
 use std::env;
 use std::time::{Duration, SystemTime};
+use hyper::client::Client;
+use hyper::header::{Accept, ContentType};
+use hyper::net::HttpsConnector;
+use hyper_native_tls::NativeTlsClient;
 
 fn main() {
 
@@ -16,6 +23,7 @@ fn main() {
   let program = args[0].clone();
 
   let mut opts = Options::new();
+  opts.optopt("u", "url", "URL to POST result to (required, also $SUMOTIME_URL)", "URL");
   opts.optopt("k", "key", "timing key (required)", "KEY");
   opts.optopt("t", "timeout", "optional timeout in seconds, after which we'll SIGKILL", "TIMEOUT");
   opts.optflag("v", "version", "print the version");
@@ -23,13 +31,13 @@ fn main() {
   let matches = match opts.parse(&args[1..]) {
     Ok(m) => { m }
     Err(f) => {
-      print!("{}", f.to_string());
+      println!("{}", f.to_string());
       process::exit(1);
     }
   };
 
   if matches.opt_present("v") {
-    print!("{}\n", version!());
+    println!(version!());
     return;
   }
 
@@ -40,13 +48,28 @@ fn main() {
   }
 
   if !matches.opt_present("k") {
-    print!("ERROR: -k/--key is required\n\n");
+    println!("ERROR: -k/--key is required\n");
     let brief = format!("Usage: {} [options]", program);
     print!("{}", opts.usage(&brief));
     process::exit(1);
   }
 
   let key = matches.opt_str("k").unwrap();
+
+  let url: String = match matches.opt_str("u") {
+    Some(v) => v,
+    None => {
+      match env::var("SUMOTIME_URL") {
+        Ok(v) => v,
+        Err(_) => {
+          println!("ERROR: -u/--url or $SUMOTIME_URL is required\n");
+          let brief = format!("Usage: {} [options]", program);
+          print!("{}", opts.usage(&brief));
+          process::exit(1);
+        }
+      }
+    }
+  };
 
   let command = &matches.free[0];
   let args = &matches.free[1..];
@@ -59,23 +82,45 @@ fn main() {
     let timeout = matches.opt_str("t").unwrap().parse::<u64>().unwrap();
     let timeout_duration = Duration::from_secs(timeout);
     match child.wait_timeout(timeout_duration).unwrap() {
-      Some(status) => finalize(key, status.code().unwrap(), false, now),
+      Some(status) => finalize(url, key, status.code().unwrap(), false, now),
       None => {
         // child hasn't exited yet
         let _ = child.kill();
         let _ = child.wait();
-        finalize(key, 124, true, now);
+        finalize(url, key, 124, true, now);
       }
     };
   } else {
-    finalize(key, child.wait().unwrap().code().unwrap(), false, now);
+    finalize(url, key, child.wait().unwrap().code().unwrap(), false, now);
   }
 
 }
 
-fn finalize(key: String, exit_code: i32, was_timeout: bool, started_at: SystemTime) {
+fn finalize(url: String, key: String, exit_code: i32, was_timeout: bool, started_at: SystemTime) {
   let duration = started_at.elapsed().unwrap().as_secs();
-  print!("key={} code={} timeout={} duration={}", key, exit_code, was_timeout, duration);
+  let body = format!("sumotime key={} code={} timeout={} duration={}", key, exit_code, was_timeout, duration);
+
+  let ssl = NativeTlsClient::new().unwrap();
+  let connector = HttpsConnector::new(ssl);
+  let client = Client::with_connector(connector);
+  match client
+    .post(url.as_str())
+    .body(&body)
+    .header(ContentType::plaintext())
+    .header(Accept::text())
+    .send() {
+    Ok(mut res) => {
+      if !res.status.is_success() {
+        println!("sumotime WARN: {}", res.status);
+        let mut res_body = String::new();
+        match res.read_to_string(&mut res_body) {
+          Ok(_) => println!("{}", res_body),
+          Err(e) => println!("error reading body: {:?}", e)
+        };
+      }
+    }
+    Err(e) => println!("sumotime Err: {:?}", e)
+  };
   process::exit(exit_code);
 }
  
