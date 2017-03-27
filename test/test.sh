8bdd5e86d18bc51e70e9b8735391120d8ce68a9a
Bob Zoller <bob@zoller.us>
2017-03-27 13:08:10 -1000

    add test for STDIN, STDOUT redirection

--- test/test.sh
+++ test/test.sh
@@ -17,9 +17,14 @@ sleep 1
 export SUMOTIME_URL="http://localhost:8080"
 
 sumotime -k 'foo.bar' false && die "expected exit code to passthru"
-[ "$(curl -sS "$SUMOTIME_URL")" = '{"msg":"sumotime","key":"foo.bar","code":1,"timeout":false,"duration":0}' ] || die "didn't POST properly"
+[ "$(curl -sS "$SUMOTIME_URL")" = '{"msg":"sumotime","key":"foo.bar","code":1,"timeout":false,"duration":0}' ] || die "didn't POST failure properly"
 
 sumotime -k 'foo.bar' -t 1 sleep 3 && die "expected timeout"
-[ "$(curl -sS "$SUMOTIME_URL")" = '{"msg":"sumotime","key":"foo.bar","code":124,"timeout":true,"duration":1}' ] || die "didn't timeout properly"
+[ "$(curl -sS "$SUMOTIME_URL")" = '{"msg":"sumotime","key":"foo.bar","code":124,"timeout":true,"duration":1}' ] || die "didn't POST timeout properly"
+
+f=$(mktemp)
+( echo 'hello world' | sumotime -k 'foo.bar' cat > "$f" ) || die "expected success"
+[ "$(cat "$f")" = "hello world" ] || die "expected STDIN, STDOUT to passthru"
+[ "$(curl -sS "$SUMOTIME_URL")" = '{"msg":"sumotime","key":"foo.bar","code":0,"timeout":false,"duration":0}' ] || die "didn't POST success properly"
 
 kill -9 "$pid"
